2244f271352c00cf5afef12eadd9e1e4c1d030e5
nwejin <147528674+nwejin@users.noreply.github.com>
2024-07-08 11:37:11 +0900

    done

---
+++ src/redux/modules/todosSlice.js
@@ -0,0 +1,53 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+/**
+ * 2초 지연을 시키는 함수입니다 (비동기 작업).
+ */
+import { waitTwoSeconds } from '../../utils';
+
+export const __addToDo = createAsyncThunk(
+  '__addToDo',
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    // console.log(payload)
+    return payload
+  }
+);
+
+export const __deleteTodo = createAsyncThunk(
+  '__deleteToDo',
+  async (payload, thunkAPI) => {
+    await waitTwoSeconds();
+    // console.log(payload)
+    return payload
+  }
+);
+
+const initialState = {
+  list: [],
+};
+
+const todosSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+     state.list = [...state.list, action.payload]; 
+      console.log(state.list)
+    },
+    deleteTodo: (state, action) => {
+      state.list = state.list.filter((todo)=> todo.id !== action.payload)
+    },
+  },
+    extraReducers: (builder) => {
+    builder.addCase(__addToDo.fulfilled, (state, action) => {
+      state.list = [...state.list, action.payload];
+    });
+    builder.addCase(__deleteTodo.fulfilled, (state, action) => {
+       state.list = state.list.filter(todo => todo.id !== action.payload);
+    })
+  },
+});
+
+export const { addTodo, deleteTodo } = todosSlice.actions;
+export default todosSlice.reducer;
